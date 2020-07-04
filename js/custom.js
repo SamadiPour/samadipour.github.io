88f8b06bed691014cf07c3dd4ba4d905d451e195
SamadiPour <samadipoor2@gmail.com>
2020-07-05 00:27:34 +0430

    rebuilding site Sun, Jul  5, 2020 00:27:33

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
-lightGallery(document.getElementById('lightgallery'), {
-    mode: 'lg-fade'
-});
+lightGallery(document.getElementById('lightgallery'), {mode: 'lg-fade'});
+lightGallery(document.getElementById('lightgallery2'), {mode: 'lg-fade'});
+lightGallery(document.getElementById('lightgallery3'), {mode: 'lg-fade'});
+lightGallery(document.getElementById('lightgallery4'), {mode: 'lg-fade'});
+lightGallery(document.getElementById('lightgallery5'), {mode: 'lg-fade'});
