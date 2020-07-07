5e58b75ca19d73d7442b6a2f09db4910fe12092c
SamadiPour <samadipoor2@gmail.com>
2020-07-07 14:08:06 +0430

    rebuilding site Tue, Jul  7, 2020 14:08:05

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
-lightGallery(document.getElementById('lightgallery'), {mode: 'lg-fade'});
-lightGallery(document.getElementById('lightgallery2'), {mode: 'lg-fade'});
-lightGallery(document.getElementById('lightgallery3'), {mode: 'lg-fade'});
-lightGallery(document.getElementById('lightgallery4'), {mode: 'lg-fade'});
-lightGallery(document.getElementById('lightgallery5'), {mode: 'lg-fade'});
+let galleries = [...document.getElementsByClassName("lightgallery")];
+galleries.forEach(item => {
+    lightGallery(item, {mode: 'lg-fade'})
+})
